f57b4bdbccf7933e1c2c1996e363f4589671d713
hyn0711 <109887064+hyn0711@users.noreply.github.com>
2023-01-16 16:16:28 +0900

    Update echo_server.py

--- network/echo_server.py
+++ network/echo_server.py
@@ -14,7 +14,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
     while True:
         client_socket, client_adder = server_socket.accept()
         msg = client_socket.recv(SIZE)
-        print("[{}] message : {}".format(client_addr,msg))
+        print("[{}] message : {}".format(client_adder,msg))
 
         client_socket.sendall("welcome!".encode())
 
