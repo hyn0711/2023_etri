0fc9cc2acc9fe73ad4d7b8687add6864e0dac183
hyn0711 <109887064+hyn0711@users.noreply.github.com>
2023-01-25 13:29:04 +0900

    Create picam_image_client_pose_0125.py

---
+++ network/picam_image_client_pose_0125.py
@@ -0,0 +1,108 @@
+
+
+import cv2
+import socket
+import numpy as np
+from picamera2 import Picamera2
+import warnings
+import time
+import torch
+import pickle
+
+warnings.filterwarnings('ignore')
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+client_socket.connect(('129.254.187.105', 8485))
+ 
+picam2 = Picamera2()
+picam2.configure(picam2.create_preview_configuration(main={"format": 'XRGB8888', "size": (640,360)}))
+picam2.start()
+
+def plot_skeleton_kpts(im, kpts, steps, orig_shape=None):
+    #Plot the skeleton and keypointsfor coco datatset
+    palette = np.array([[255, 128, 0], [255, 153, 51], [255, 178, 102],
+                        [230, 230, 0], [255, 153, 255], [153, 204, 255],
+                        [255, 102, 255], [255, 51, 255], [102, 178, 255],
+                        [51, 153, 255], [255, 153, 153], [255, 102, 102],
+                        [255, 51, 51], [153, 255, 153], [102, 255, 102],
+                        [51, 255, 51], [0, 255, 0], [0, 0, 255], [255, 0, 0],
+                        [255, 255, 255]])
+
+    skeleton = [[16, 14], [14, 12], [17, 15], [15, 13], [12, 13], [6, 12],
+                [7, 13], [6, 7], [6, 8], [7, 9], [8, 10], [9, 11], [2, 3],
+                [1, 2], [1, 3], [2, 4], [3, 5], [4, 6], [5, 7]]
+
+    pose_limb_color = palette[[9, 9, 9, 9, 7, 7, 7, 0, 0, 0, 0, 0, 16, 16, 16, 16, 16, 16, 16]]
+    pose_kpt_color = palette[[16, 16, 16, 16, 16, 0, 0, 0, 0, 0, 0, 9, 9, 9, 9, 9, 9]]
+    radius = 5
+    num_kpts = len(kpts) // steps
+
+    for kid in range(num_kpts):
+        r, g, b = pose_kpt_color[kid]
+        x_coord, y_coord = kpts[steps * kid], kpts[steps * kid + 1]
+        if not (x_coord % 640 == 0 or y_coord % 640 == 0):
+            if steps == 3:
+                conf = kpts[steps * kid + 2]
+                if conf < 0.5:
+                    continue
+            cv2.circle(im, (int(x_coord), int(y_coord)), radius, (int(r), int(g), int(b)), -1)
+
+    for sk_id, sk in enumerate(skeleton):
+        r, g, b = pose_limb_color[sk_id]
+        pos1 = (int(kpts[(sk[0]-1)*steps]), int(kpts[(sk[0]-1)*steps+1]))
+        pos2 = (int(kpts[(sk[1]-1)*steps]), int(kpts[(sk[1]-1)*steps+1]))
+        if steps == 3:
+            conf1 = kpts[(sk[0]-1)*steps+2]
+            conf2 = kpts[(sk[1]-1)*steps+2]
+            if conf1<0.5 or conf2<0.5:
+                continue
+        if pos1[0]%640 == 0 or pos1[1]%640==0 or pos1[0]<0 or pos1[1]<0:
+            continue
+        if pos2[0] % 640 == 0 or pos2[1] % 640 == 0 or pos2[0]<0 or pos2[1]<0:
+            continue
+        cv2.line(im, pos1, pos2, (int(r), int(g), int(b)), thickness=2)
+
+
+
+encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
+
+
+while True:
+
+    frame1 = picam2.capture_array()
+    frame = frame1.copy()
+    result, frame = cv2.imencode('.jpg', frame, encode_param)
+    
+    #print(type(frame1))
+
+    data = np.array(frame)
+    stringData = data.tostring()
+
+    client_socket.sendall((str(len(stringData))).encode().ljust(16) + stringData)
+    
+    output1 = client_socket.recv(4096)
+    output = pickle.loads(output1)
+    #output = np.fromstring(output1, dtype = 'float64')
+    
+    print(output)
+    print(output.ndim)
+    print(type(output))
+    print(frame1.ndim)
+    print(type(frame1))
+    # output : 2 dim , numpy.ndarray
+    # frame1 : 3 dim, numpy.ndarray
+    
+    if output.shape[0] > 0:
+        for idx in range(output.shape[0]):
+            plot_skeleton_kpts(frame1, output[idx, 7:].T, 3)
+    start = time.time()
+    end = time.time()
+    fps = 1 - (end-start)
+    start = end
+    cv2.putText(frame1, "FPS : %0.3f"%fps, (7,70), cv2.FONT_HERSHEY_SIMPLEX, 1, (100,255,0), 3, cv2.LINE_AA )
+    cv2.putText(frame1, "%d detected"%output.shape[0], (7,90), cv2.FONT_HERSHEY_SIMPLEX, 1, (100,255,0), 3, cv2.LINE_AA)
+    cv2.imshow('pose', frame1)
+    
+    cv2.waitKey(1)
+client_socket.close()
